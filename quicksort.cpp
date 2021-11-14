8a0dd32199410f8567d15eaeaefcaa6d136ea5dc
Joel Crouch <joelcrouch@gmail.com>
2021-11-13 21:26:49 -0800

    add existing quicksort file

---
+++ quicksort.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+
+//swap function required for many algorithm
+void swap(int * x, int * y){
+    int temp= * x;
+    *x = *y;
+    *y=temp;
+}
+
+int split(int array[], int low, int last)//take in array, and low, high index
+{
+    int pivot = array[last];
+    int i=(low-1);
+    for(int j=low; j<=last -1; j++){
+        if(array[j]<pivot){
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i+1], &array[last]);
+    return (i+1);
+}   
+
+void quicksort(int array[], int low, int last){
+    if(low<last){
+
+        int splitup=split(array, low, last);
+        quicksort(array, low, splitup-1);
+        quicksort(array, splitup+1, last);
+    }
+}
+
+//need driver for test, maybe a print function to make it easy
+
