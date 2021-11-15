1d9d09a82b24a36fcc449410200d4546656398e5
Joel Crouch <joelcrouch@gmail.com>
2021-11-14 21:03:01 -0800

    it g

---
+++ tree/tree.h
@@ -0,0 +1,36 @@
+#include <cstring>
+#include <iostream>
+#include <cctype>
+#include <cstdlib>
+using namespace std;
+
+struct node{
+    int data;
+    node * right;
+    node * left;
+
+    Node(int input){
+        data = input;
+        left=right=NULL;
+    }
+};
+
+class tree{
+    public:
+        tree();
+        ~tree();
+        int add(int toAdd);
+        int height();
+        int height(node * root);
+        int build(node * & root, int toAdd);
+        bool display();
+        bool display(node * root);
+
+    private:
+        node *root;
+
+
+
+
+
+}
